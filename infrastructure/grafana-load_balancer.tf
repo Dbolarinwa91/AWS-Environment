bc911baca1c4da8727e565f47ad38dd22555e06b
David-Bolarinwa <dbolarinwa91@gmail.com>
2025-04-14 14:11:15 -0500

    t

--- infrastructure/grafana-load_balancer.tf
+++ infrastructure/grafana-load_balancer.tf
@@ -8,36 +8,31 @@ resource "aws_lb_target_group" "grafana_tg" {
   
   health_check {
     enabled             = true
-    interval            = 30
-    path                = "/api/health"
+    interval            = 15
+    path                = "/"
     port                = "traffic-port"
     healthy_threshold   = 3
     unhealthy_threshold = 3
     timeout             = 5
     matcher             = "200"
   }
+    # Allow time for connections to drain before deregistering
+  deregistration_delay = 60
+  
+  # Enable stickiness for better user experience
+  stickiness {
+    type            = "lb_cookie"
+    cookie_duration = 259200  # 3 day
+    enabled         = true
+  }
+  
   
   tags = {
     Name = "grafana-target-group-devops-David-site-project"
   }
 }
 
-# Listener Rule (Path-based routing)
-resource "aws_lb_listener_rule" "grafana" {
-  listener_arn = aws_lb_listener.sonarqube.arn  # Using existing listener
-  priority     = 100  # Priority that doesn't conflict with existing rules
-  
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.grafana_tg.arn
-  }
-  
-  condition {
-    path_pattern {
-      values = ["/grafana*"]  # Path-based routing for subpath
-    }
-  }
-}
+
 
 # Dedicated Listener
 resource "aws_lb_listener" "grafana" {
