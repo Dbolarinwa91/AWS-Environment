ffd94eb352a4c47248b1836db941c974b5820f6e
David-Bolarinwa <dbolarinwa91@gmail.com>
2025-04-14 13:13:55 -0500

    rerun

--- infrastructure/grafana-iam.tf
+++ infrastructure/grafana-iam.tf
@@ -9,3 +9,23 @@ resource "aws_ssm_parameter" "grafana_admin_password" {
     Name = "grafana-admin-password-devops-David-site-project"
   }
 }
+# Add SSM Parameter access to the ECS Task Execution Role
+resource "aws_iam_role_policy" "ecs_task_execution_ssm" {
+  name   = "ecs-task-execution-ssm-policy"
+  role   = aws_iam_role.ecs_task_execution_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        Resource = [
+          "arn:aws:ssm:us-east-1:061652678349:parameter/grafana/admin-password"
+        ]
+      }
+    ]
+  })
+}
