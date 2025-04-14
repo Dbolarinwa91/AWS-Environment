9fa9eb7995dace10e1bc02beda9d85db689fba17
David-Bolarinwa <dbolarinwa91@gmail.com>
2025-04-14 11:56:55 -0500

    rerun

--- infrastructure/database.tf
+++ infrastructure/database.tf
@@ -224,25 +224,3 @@ resource "aws_efs_access_point" "sonarqube_extensions" {
   }
 }
 
-# EFS Access Point
-resource "aws_efs_access_point" "grafana_data" {
-  file_system_id = aws_efs_file_system.sonarqube.id  # Reusing existing EFS
-  
-  posix_user {
-    gid = 472  # Default Grafana UID
-    uid = 472  # Default Grafana GID
-  }
-  
-  root_directory {
-    path = "/grafana-data"
-    creation_info {
-      owner_gid   = 472
-      owner_uid   = 472
-      permissions = "755"
-    }
-  }
-  
-  tags = {
-    Name = "grafana-data-access-point-devops-David-site-project"
-  }
-}
